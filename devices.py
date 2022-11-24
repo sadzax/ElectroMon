e02b88150eb28bc709fd1943775d0c014708707a
Marat Ramazanov <sadzax@gmail.com>
2022-11-24 13:57:57 +0300

    1.2

--- devices.py
+++ devices.py
@@ -27,18 +27,44 @@ def preview(file, limit):
                 count += 1
 
 
-work_file = 'DB_i.csv'
+work_file = 'DB_i.csv'  # goal file
+work_file_sep = ';'
+work_file_default_encoding = 'WINDOWS-1251'
+work_file_parse_dates = ['Дата создания записи', 'Дата сохранения в БД']
 
 
-def get_data(file):
-    data = pd.read_csv(file,
-                       sep=';',
-                       encoding='WINDOWS-1251',
-                       parse_dates=['Дата создания записи', 'Дата сохранения в БД'],
+def get_cols(file=work_file, sep=work_file_sep, encoding=work_file_default_encoding, parse_dates=work_file_parse_dates):
+    col_array = pd.read_csv(file=file,
+                            sep=sep,
+                            encoding=encoding,
+                            parse_dates=parse_dates,
+                            dayfirst=True,
+                            nrows=1).columns
+    return col_array
+
+def get_data(usecols: list = None, file=work_file, sep=work_file_sep, encoding=work_file_default_encoding):
+    if usecols is None:
+        parse_dates = work_file_parse_dates
+    else:
+        cols = []
+        for k in usecols:
+            if k in work_file_parse_dates:
+                cols.append(k)
+        parse_dates = cols
+    data = pd.read_csv(file=file,
+                       sep=sep,
+                       encoding=encoding,
+                       parse_dates=parse_dates,
+                       usecols=usecols,
                        dayfirst=True)
     return data
 
 
-df = get_data(work_file)
-print(df)
-
+def plot_2d_simple(val_x: str, val_y: str, size_x: int = 14, size_y: int = 4):
+    df = get_data([val_x, val_y])
+    fig, axs = plt.subplots(figsize=(size_x, size_y))
+    plt.xlabel(val_x)
+    plt.ylabel(val_y)
+    x = df[val_x].tolist()
+    y = df[val_y].tolist()
+    axs.plot(x, y)
