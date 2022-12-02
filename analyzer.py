e0f1af7c5d7a1317a7dbc49d5145a6831552c6c5
Marat Ramazanov <sadzax@gmail.com>
2022-12-03 02:32:41 +0300

    1.8 Replaced delta_tg_checker()

--- analyzer.py
+++ analyzer.py
@@ -4,12 +4,12 @@ import columns
 import devices
 import itertools
 
-#  Однократное получение данных (* уточнить по оптимизации, нужно ли)
+#  Get variable for data
 cols = columns.columns_analyzer()
 cols_len = len(columns.columns_maker())
 
 
-#  Получение данных из CSV-файла
+#  Importing CSV
 def get_data(usecols: list = None,
              file=devices.nkvv.work_file,
              sep=devices.nkvv.work_file_sep,
@@ -31,16 +31,16 @@ def get_data(usecols: list = None,
     return data
 
 
-#  Однократное получение данных в переменную
+#  Get variable for data
 database = get_data()
 
 
-#  Подсчёт общего количества строк
+#  Count the strings
 def total_log_counter(data: pd.core = database):
     return data.shape[0]
 
 
-#  Анализ времени замеров
+#  Analysis of time of measurements
 def values_time_analyzer(col_number=0,
                          time_sequence_min=1,
                          cl=cols,
@@ -69,8 +69,8 @@ def values_time_analyzer(col_number=0,
                   f", т.е. через {err}\n")
 
 
-#  Перевести исключения (Ia(r) = -300, Tg = -10) в NaN
-def pass_the_nan(seeking_param='power',  # Слишком медленно работает, много циклов, надо упростить
+#  Exclude (Ia(r) = -300, Tg = -10) to NaN
+def pass_the_nan(seeking_param='power',  # Need to optimize memory usage
                  replacing_value=-300.0,
                  cl=cols,
                  data: pd.core = database):
@@ -84,17 +84,19 @@ def pass_the_nan(seeking_param='power',  # Слишком медленно ра�
 
 
 database = pass_the_nan('power', -300.0)
-database = pass_the_nan('tg', -10.0)
+database = pass_the_nan('tg', -10.0)  # ?? Need to optimize
 database = pass_the_nan('∆tgδ', -10.0)
 
 
-#  ______ Корреляция с температурой окружающей среды (п.3.1. отчёта)
+#  ______ Correlation with environment temperature (p.3.1. of the report)
 def correlation_temp():
     pass
 
 
-#  Фильтрация
-def data_filter(filter_list, data=database, cl=cols):
+#  Filtering
+def data_filter(filter_list,
+                data=database,
+                cl=cols):
     filter_list_indexes = []
     for a_column in range(cols_len):
         for a_param in range(len(cols[0])):
@@ -104,12 +106,21 @@ def data_filter(filter_list, data=database, cl=cols):
     return data[filter_list_names]
 
 
-def data_average_finder(filter_list, abs_parameter=True, unite_parameter=False, list_of_non_math=None, data=database, cl=cols):
+# Main averager
+def data_average_finder(filter_list=None,
+                        abs_parameter=True,
+                        unite_parameter=False,
+                        list_of_non_math=None,
+                        data=database,
+                        cl=cols):
+    if filter_list is None:
+        filter_list = ['time', '∆tgδ_HV']
     if list_of_non_math is None:
         list_of_non_math = ['Дата создания записи',
                             'Дата сохранения в БД']
     df = data_filter(filter_list, data, cl)
     func_columns_list = list(df.columns)
+    func_result_prev = []
     func_result = {}
     for i in range(df.shape[1]):
         for k in list_of_non_math:
@@ -117,23 +128,25 @@ def data_average_finder(filter_list, abs_parameter=True, unite_parameter=False, 
                 break
         else:
             columns_list_of_values = df[func_columns_list[i]].tolist()
-            if unite_parameter is True:  # !!! _ DEBUG
-                values_without_nan = []
-                if abs_parameter is True:
-                    values_without_nan.append(abs(x) for x in columns_list_of_values if not np.isnan(x))
-                else:
-                    values_without_nan.append(x for x in columns_list_of_values if not np.isnan(x))
-                func_result = {filter_list: sum(values_without_nan)/len(values_without_nan)}
-            else:
+            # okay
+            if unite_parameter is False:
                 if abs_parameter is True:
                     values_without_nan = [abs(x) for x in columns_list_of_values if not np.isnan(x)]
                 else:
                     values_without_nan = [x for x in columns_list_of_values if not np.isnan(x)]
                 func_result[func_columns_list[i]] = sum(values_without_nan)/len(values_without_nan)
+            else:
+                if abs_parameter is True:
+                    dump = [abs(x) for x in columns_list_of_values if not np.isnan(x)]
+                else:
+                    dump = [x for x in columns_list_of_values if not np.isnan(x)]
+                func_result_prev = func_result_prev + dump
+                func_result = {'Среднее по всем: ': sum(func_result_prev) / len(func_result_prev)}
     return func_result
 
 
-def data_average_finder_2(filter_list, data=database, cl=cols):
+#  ______ Search for deviations
+def data_deviation_finder(filter_list, data=database, cl=cols):
     df = data_filter(filter_list, data, cols)
     func_columns_list = list(df.columns)
     for i in range(df.shape[1] - 1):
@@ -146,13 +159,13 @@ def data_average_finder_2(filter_list, data=database, cl=cols):
 print(data_average_finder(['time', '∆tgδ_HV']))
 
 
-#  ______ Проверка параметра ∆tgδ для срабатывания предупредительной сигнализации (1%)
-def delta_tg_checker(cl=cols,  # Добавить индексы и оперировать словарём (с датами и временем)
+#  ______ Проверка параметра ∆tgδ для технических целей
+def delta_tg_checker(cl=cols,
                      data: pd.core = database,
                      exclude_values=(-10.0, -300.0)):
     df = []
     for column_index in range(cols_len):
-        if cl[column_index][4] == '∆tgδ' and cl[column_index][3] == 'HV':  # заменить фильтры на формулы
+        if cl[column_index][4] == '∆tgδ' and cl[column_index][3] == 'HV':
             df.append(data[cl[column_index][0]].tolist())
     list_of_all_values = list(itertools.chain.from_iterable(df))
     list_of_filtered_values = []
@@ -163,10 +176,7 @@ def delta_tg_checker(cl=cols,  # Добавить индексы и оперир
     return list_of_filtered_abs_values
 
 
-delta_tg_HV_check = delta_tg_checker()
-
-
-#  ______ Проверка срабатывания сигнализации
+#  ______ Проверка срабатывания сигнализации срабатывания предупредительной сигнализации (1%)
 def delta_tg_checker_warning(operating_data=None, warning=1):
     if operating_data is None:
         operating_data = delta_tg_HV_check
@@ -181,7 +191,9 @@ def delta_tg_checker_warning(operating_data=None, warning=1):
         return warning_list
 
 
-#  Выводы
+delta_tg_HV_check = delta_tg_checker()
+
+
 print(values_time_analyzer())
 print(f"\nОбщее число записей в журнале измерений составило {total_log_counter()}")
 
