6b687adb58a1209dfac6fec07d1059580ae3e499
Marat Ramazanov <sadzax@gmail.com>
2023-01-06 00:48:46 +0300

    kiv start

--- temp_exec_kiv.py
+++ temp_exec_kiv.py
@@ -1,25 +1,29 @@
 import os
 import devices
 import pandas as pd
+import warnings
+import columns
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 kiv = devices.Device()
 devices.kiv.work_file_folder = 'upload/kiv/'
 devices.kiv.file_names_starts = {'measure': 'MeasJ', 'event': 'WorkJ'}
 
-a = [filename for filename in os.listdir(devices.kiv.work_file_folder)
-     if filename.startswith(devices.kiv.file_names_starts['measure'])]
-for i in a:
-    measj = ("upload/kiv/" + i)
 
-measj = ('upload/kiv/' + a[0])
+def kiv_xlsx_files_form():
+    a_list_of_files = [filename for filename
+         in os.listdir(devices.kiv.work_file_folder)if filename.startswith(devices.kiv.file_names_starts['measure'])]
+    return a_list_of_files
 
+
+measj = ('upload/kiv/' + kiv_xlsx_files_form()[0])
 df = pd.read_excel(measj)
 
 if df.iloc[0, 0] == 'Модуль:':
     devices.kiv.serial_number = df.iloc[0, 3]
 
-def kiv_columns_maker():
-    if df.iloc[0, 0] == 'Модуль:':
-        df.columns = df.iloc[3]
+headers = df.iloc[3]
+df = pd.DataFrame(df.values[4:], columns=headers)
+cols = list(headers)
+cols_dict = columns.dict_maker(cols)
 
-columns = df.iloc[3]
