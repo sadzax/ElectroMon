3366f2fe4081b55c6229ff0be0514138f67d037d
Marat Ramazanov <sadzax@gmail.com>
2023-01-08 21:55:06 +0300

    kiv columns debugging

--- temp_exec_kiv.py
+++ temp_exec_kiv.py
@@ -19,8 +19,20 @@ def kiv_xlsx_files_form():
     return a_list_of_files
 
 
+def dict_maker(list_for_columns=None,
+               file=devices.nkvv.work_file,
+               sep=devices.nkvv.work_file_sep,
+               encoding=devices.nkvv.work_file_default_encoding):
+    if list_for_columns is None:
+        list_for_columns = columns_maker(file=file, sep=sep, encoding=encoding)
+    return {v: [k] for v, k in enumerate(list_for_columns)}
+
+
 measj = 'upload/kiv/' + kiv_xlsx_files_form()[0]
-df = pd.read_excel(measj)
+# df = pd.read_excel(measj)
+# df.to_pickle('main_dataframe_kiv.pkl')
+
+df = pd.read_pickle('main_dataframe.pkl')
 
 if df.iloc[0, 0] == 'Модуль:':
     devices.kiv.serial_number = df.iloc[0, 3]
