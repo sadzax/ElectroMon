c7d1860c5c28c20f1db09a9da7a599d623a6e142
Marat Ramazanov <sadzax@gmail.com>
2023-03-18 19:42:21 +0300

    PDF uploads

--- temp.py
+++ temp.py
@@ -19,6 +19,7 @@ dev = device_type
 data = devices.Pkl.load(dev)
 cols_list = columns.columns_list_maker(dev, data)
 cols = columns.columns_analyzer(dev, cols_list)
+data = analyzer.set_dtypes(device_type=device_type, data=data, cols=cols)
 del cols_list
 # ___
 build_list = []
@@ -26,7 +27,6 @@ build_list = []
 capture = devices.links(device_type)[9]
 build_temp = frontend.PDF.text(capture, frontend.style_title2)
 frontend.PDF.add_to_build_list(build_temp, build_list)
-del capture
 
 cols_df = columns.columns_df(dev, cols)
 build_temp = frontend.PDF.table_from_df(cols_df, title='Список данных файла с раскладкой для анализа')
@@ -53,5 +53,56 @@ build_temp = frontend.PDF.table_from_df(total_nan_counter_ease,
                                         colWidths=[70, 60, 40, 60, 40, 100])
 frontend.PDF.add_to_build_list(build_temp, build_list)
 
+
+ex1 = '∆C'
+ex2 = '∆tg'
+
+buffer = frontend.capture_on()
+prints.info('Анализ трендов')
+capture = frontend.capture_off(buffer)
+build_temp = frontend.PDF.text(capture, frontend.style_title2)
+frontend.PDF.add_to_build_list(build_temp, build_list)
+
+
+buffer = frontend.capture_on()
+print(f'Анализ корреляции данных {ex1} от температуры воздуха (при корреляции изменения на графике синхронны)')
+plots.correlation_plot(filter_list1=[ex1], filter_list2=['tair'], device_type=device_type, data=data, cols=cols,
+                       title=f"Анализ корреляции данных {ex1} от температуры воздуха")
+capture = frontend.capture_off(buffer)
+build_temp = frontend.PDF.text(capture, frontend.style_title2)
+frontend.PDF.add_to_build_list(build_temp, build_list)
+
 # ____________
 frontend.PDF.builder(build_list, 'output.pdf')
+
+
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+import matplotlib.pyplot as plt
+
+# create a ReportLab PDF canvas
+pdf_canvas = canvas.Canvas("example.pdf", pagesize=letter)
+
+# create a matplotlib figure
+fig = plt.figure(figsize=(5, 4))
+ax = fig.add_subplot(111)
+ax.plot([1, 2, 3, 4], [10, 20, 30, 40])
+
+# create a buffer to hold the plot image
+buf = io.BytesIO()
+
+# render the matplotlib figure to the buffer
+canvas = FigureCanvasAgg(fig)
+canvas.print_figure(buf, format='png')
+
+# reset the buffer to the beginning so it can be read
+buf.seek(0)
+
+# create a ReportLab PDF image from the buffer
+pdf_image = pdf_canvas.drawImage(buf, 100, 100)
+
+# save the PDF canvas
+pdf_canvas.save()
+
